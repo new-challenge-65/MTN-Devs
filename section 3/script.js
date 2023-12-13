4f46a72ba82964d25480c5d7f749aef45792ec04
Mohadese-Keshtkar <mohadesekeshtkar4@gmail.com>
2023-12-13 16:26:19 +0330

    add section 2 folder

---
+++ section 3/script.js
@@ -0,0 +1,53 @@
+
+var options = {
+    chart: {
+        type: 'area',
+        toolbar: {
+            show: false,
+        },
+        zoom: {
+            enabled: false,
+            type: 'x',
+            autoScaleYaxis: false,
+            zoomedArea: {
+                fill: {
+                    opacity: 0.4,
+                    color: '#f1416d23',
+                },
+                stroke: {
+                    color: ['#f1416c'],
+                    opacity: 0.4,
+                    width: 1
+
+                },
+            }
+        }
+    },
+    series: [{
+        name: 'sales',
+        data: [80, 60, 80, 70, 80, 70, 90, 90, 90],
+        show: false
+    }],
+    xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    },
+    stroke: {
+        curve: 'smooth',
+    },
+    grid: {
+        show: false
+    },
+    yaxis: {
+        show: false,
+        xaxis: {
+            show: false
+        }
+    },
+    dataLabels: {
+        enabled: false
+    }
+
+}
+
+var chart = new ApexCharts(document.querySelector("#myChart"), options);
+chart.render();
