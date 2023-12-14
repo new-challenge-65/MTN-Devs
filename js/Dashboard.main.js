71192633faf9dacf68505ff71a78ed41433d34c0
EngineerAmin6886 <127695649+EngineerAmin6886@users.noreply.github.com>
2023-12-14 22:42:41 +0330

    section(tagvim)
    1:ezafeh kardan tagvim v takmil gozehat.
    2:ezafeh kardan ghsmat payani footer.
    3:tamam shodan en ghsmat.

--- js/Dashboard.main.js
+++ js/Dashboard.main.js
@@ -2,15 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    // initialView: "dayGridMonth,",
     events: "/api/demo-feeds/events.json",
     editable: true,
     selectable: true,
     headerToolbar: {
       start: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
       center: "title",
-      // end: 'prevYear,prev,next,nextYear'
     },
+    events:[{
+      title: 'mehndi',
+      start:'2023-12-14',
+      end:'2023-12-14T13:00',
+      color:'blue',
+      textColor:'black',
+    },
+    {
+      title:'haldi',
+      start:'2023-12-20T13:00',
+      end:'2023-12-20T18:00',
+      color:'blue',
+      background:'yellow',
+      textColor:'black'
+    }
+  ]
   });
 
   calendar.render();
